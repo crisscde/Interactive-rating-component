075ae83a7f60aad11160c33b0276a2242ded2754
Crisscde <cristopher-12uriel@hotmail.com>
2022-04-30 00:12:33 -0500

    Layout in CSS and HTML

---
+++ README.md
@@ -0,0 +1,2 @@
+# Interactive grading component
+Hi, this is an exercise from [Frontend Mentor](https://www.frontendmentor.io/home). This is my firts project using Vanilla JS, apreciate your feedback    
---
+++ script.js
@@ -0,0 +1,9 @@
+const button_rank = document.querySelectorAll(".rate__button")
+const raking_text = document.querySelector(".card__ranking")
+const ranking = document.querySelector("#card__raking")
+const thanks = document.querySelector("#card__thanks")
+
+raking_text.textContent = `You Selected out of 5`;
+raking_text.textContent;
+
+thanks.style.cssText = "display: none";
