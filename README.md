3badabd594d0c88b41126f071b64a406e31726c3
Crisscde <cristopher-12uriel@hotmail.com>
2022-04-30 17:32:57 -0500

    This is the interactive component

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Interactive grading component
 Hi, this is an exercise from [Frontend Mentor](https://www.frontendmentor.io/home). This is my firts project using Vanilla JS, apreciate your feedback    
+
+## [Live Site](https://interactive-rating-component-orcin.vercel.app/)
+  ### Desktop
+  ![Desktop-Live](LiveSite/Desktop-View.png)
+  ### Phone
+  ![Desktop-Live](LiveSite/Phone-View.png)
--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const button_rank = document.querySelectorAll(".rate__button")
-const raking_text = document.querySelector(".card__ranking")
-const ranking = document.querySelector("#card__raking")
-const thanks = document.querySelector("#card__thanks")
+const button_rank = document.querySelectorAll(".rate__button");
+const raking_text = document.querySelector(".card__rankingText");
+const ranking = document.querySelector("#card__ranking");
+const thanks = document.querySelector("#card__thanks");
+const button = document.querySelector(".card__button");
+const cards = document.querySelector("#cards")
 
-raking_text.textContent = `You Selected out of 5`;
-raking_text.textContent;
+cards.removeChild(thanks)
 
-thanks.style.cssText = "display: none";
+button_rank.forEach(button => {
+  button.addEventListener("click", e => {
+    raking_text.textContent =  `You Selected ${e.path[0].innerText} out of 5`
+    raking_text.textContent;  
+  })
+});
+
+button.addEventListener("click", function(){
+  cards.removeChild(ranking)
+  cards.appendChild(thanks)
+})
