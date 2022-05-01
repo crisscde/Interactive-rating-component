290202279c8d47b8cbb4ad34181c0584c967c160
Crisscde <cristopher-12uriel@hotmail.com>
2022-05-01 16:11:08 -0500

    You can´t pass to the next card if you don't push any button

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const raking_text = document.querySelector(".card__rankingText");
 const ranking = document.querySelector("#card__ranking");
 const thanks = document.querySelector("#card__thanks");
 const button = document.querySelector(".card__button");
-const cards = document.querySelector("#cards")
+const cards = document.querySelector("#cards");
 
-cards.removeChild(thanks)
+cards.removeChild(thanks);
 
-button_rank.forEach(button => {
-  button.addEventListener("click", e => {
-    raking_text.textContent =  `You Selected ${e.path[0].innerText} out of 5`
-    raking_text.textContent;  
-  })
+button_rank.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    raking_text.textContent = `You Selected ${e.path[0].innerText} out of 5`;
+    raking_text.textContent;
+  });
 });
 
-button.addEventListener("click", function(){
-  cards.removeChild(ranking)
-  cards.appendChild(thanks)
-})
+button.addEventListener("click", function () {
+  if (raking_text.textContent === "") return;
+  cards.removeChild(ranking);
+  cards.appendChild(thanks);
+});
